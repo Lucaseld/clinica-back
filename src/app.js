6a1ed2863a7a31ef943d60293dc44c58719ecdef
Lucas <89759633+Lucaseld@users.noreply.github.com>
2023-11-18 08:01:57 -0300

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import pacientesRoutes from './routes/pacientes.routes.js'
+import medicosRoutes from './routes/medicos.routes.js'
+import turnosRoutes from './routes/turnos.routes.js'
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '127.0.0.1:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+})
+
+app.use(express.json())
+app.use(pacientesRoutes);
+app.use(medicosRoutes);
+app.use(turnosRoutes);
+
+export default app;
---
+++ src/database/database.js
@@ -0,0 +1,16 @@
+import Sequelize from 'sequelize';
+
+const password = process.env.PASSWORS
+
+//export const sequelize = new Sequelize('clinica', 'postgres', '1234', {
+export const sequelize = new Sequelize('clinicadb', 'fl0user', 'MVCf14EqjtkY', {
+    host: 'ep-square-butterfly-96990976.ap-southeast-1.aws.neon.fl0.io',
+    //host: '127.0.0.1',
+    dialect:'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
+});
