39b3283d03a4ba779374bd68b05163f0d5152c7e
Lucas <89759633+Lucaseld@users.noreply.github.com>
2023-11-18 08:14:41 -0300

    Update database.js

--- src/database/database.js
+++ src/database/database.js
@@ -3,7 +3,7 @@ import Sequelize from 'sequelize';
 const password = process.env.PASSWORD
 
 //export const sequelize = new Sequelize('clinica', 'postgres', '1234', {
-export const sequelize = new Sequelize('clinicadb', 'fl0user', password, {
+export const sequelize = new Sequelize('clinicadb', 'fl0user', 'MVCf14EqjtkY', {
     host: 'ep-square-butterfly-96990976.ap-southeast-1.aws.neon.fl0.io',
     //host: '127.0.0.1',
     dialect:'postgres',
