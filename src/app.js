02e67060f22b3b7c09ed50de519ca051f0e8651d
Lucas <89759633+Lucaseld@users.noreply.github.com>
2023-11-18 10:17:06 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://192.168.247.124:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'http://192.168.247.124');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
