c5446a2f0207fc09617a1b76aaa38dff97deb774
Lucas <89759633+Lucaseld@users.noreply.github.com>
2023-11-18 14:08:15 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://clinica-front-nu.vercel.app/');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
